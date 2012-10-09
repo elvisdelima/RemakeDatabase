a357ddbc3be282ce2b23f179dc4eb300cda65028
Alberto Monteiro <betimbrasil@gmail.com>
2012-10-09 11:28:52 -0300

    initial commit

---
+++ RemakeDatabase/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace RemakeDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dbName;
+            if (args.Any())
+            {
+                dbName = args.First();
+            }
+            else
+            {
+                Console.WriteLine("Insira o nome do banco:");
+                dbName = Console.ReadLine();
+            }
+            var sqlCloseConnections = string.Format(@"ALTER DATABASE {0} SET SINGLE_USER WITH ROLLBACK IMMEDIATE", dbName);
+            var sqlDropDatabase = string.Format(@"DROP DATABASE [{0}]", dbName);
+            var sqlCreateDatabase = string.Format(@"CREATE DATABASE [{0}]", dbName);
+
+            const string connectionString = @"Data Source=.\sqlexpress;Initial Catalog=master;Integrated Security=true";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    Console.WriteLine("Abrindo conexão com banco de dados");
+                    connection.Open();
+                    Console.WriteLine("Conexão aberta");
+                }
+                catch (Exception e)
+                {
+                    ReportErrorAndCloseConnection(e, connection);
+                    return;
+                }
+                Console.WriteLine();
+                using (var command = new SqlCommand(sqlCloseConnections, connection))
+                {
+                    try
+                    {
+                        ExecuteAndReportProcess(dbName, command, "Removendo conexões existentes com o banco de dados", "Conexões removidas");
+                    }
+                    catch (Exception e)
+                    {
+                        ReportErrorAndCloseConnection(e, connection);
+                        return;
+                    }
+                }
+                Console.WriteLine();
+                using (var command = new SqlCommand(sqlDropDatabase, connection))
+                {
+                    try
+                    {
+                        ExecuteAndReportProcess(dbName, command, "Removendo banco de dados", "Banco de dados removido");
+                    }
+                    catch (SqlException e)
+                    {
+                        if (e.Number == 3701)
+                            Console.WriteLine("Banco de dados {0} não existe, continuado para processo de criação", dbName);
+                        else
+                        {
+                            ReportErrorAndCloseConnection(e, connection);
+                            return;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        ReportErrorAndCloseConnection(e, connection);
+                        return;
+                    }
+                }
+                Console.WriteLine();
+                using (var command = new SqlCommand(sqlCreateDatabase, connection))
+                {
+                    try
+                    {
+                        ExecuteAndReportProcess(dbName, command, "Criando banco de dados", "Banco de dados criado");
+                    }
+                    catch (Exception e)
+                    {
+                        ReportErrorAndCloseConnection(e, connection);
+                    }
+                }
+                connection.Close();
+            }
+        }
+
+        private static void ExecuteAndReportProcess(string dbName, SqlCommand command, string beforeProgess, string afterProgess)
+        {
+            Console.WriteLine("{0} {1}", beforeProgess, dbName);
+            command.ExecuteNonQuery();
+            Console.WriteLine("{0}", afterProgess);
+        }
+
+        private static void ReportErrorAndCloseConnection(Exception e, SqlConnection connection)
+        {
+            Console.WriteLine(e.Message);
+            connection.Close();
+        }
+    }
+}
